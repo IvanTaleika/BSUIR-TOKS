cfa3e74772171171fc1f8abed8477382f1ce3a49
Ivan <xefzar@gmail.com>
2018-11-03 10:58:18 +0300

    added lab3

---
+++ lab3/lab3/serialport.h
@@ -0,0 +1,27 @@
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
+#include <QtSerialPort/QSerialPort>
+#include "serialportexception.h"
+
+class SerialPort : public QSerialPort {
+ public:
+  SerialPort(QObject* parent);
+  QByteArray readPackage();
+  qint64 writePackage(QByteArray array);
+ private:
+  static const QByteArray FIRST_BYTE;
+  static const QByteArray ESCAPE_BYTE;
+  static const QByteArray FIRST_REPLACEMENT;
+  static const QByteArray ESCAPE_REPLACEMENT;
+  static const int BITS_IN_BYTE;
+  static const QString CRC8;
+
+  void code(QByteArray& array);
+  void decode(QByteArray& array);
+  QString toBitString(QByteArray& array);
+  int calcCrc(QString bitString);
+  void validateCrc(QByteArray& array);
+
+};
+
+#endif // SERIALPORT_H
